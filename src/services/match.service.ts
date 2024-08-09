c7ec6b6ac99c0e903c9efb184ae339c6de22e773
Raúl Jiménez Galán <truenodeveloper@gmail.com>
2024-08-09 03:31:55 +0200

    +Added devmode for rendered HTML URLs

--- src/services/match.service.ts
+++ src/services/match.service.ts
@@ -1,11 +1,15 @@
 import { captureImage } from "./imageCapture"
 import fs from "fs"
+import dotenv from "dotenv"
+dotenv.config()
 
+const DEVMODE = process.env.DEVMODE || false;
+const BASE_URL = DEVMODE ? "https://www.caracantosmeaos.club" : "http://localhost:4321"
 
 const getMatchImage = async (matchid: number|string) => {
     const filepath = `./imagescache/match_${matchid}.jpeg`
     if(!fs.existsSync(filepath)){
-        await captureImage("http://localhost:4321/imagegenerator/matchsummary/"+matchid, "match_"+matchid, "#to_image", 1920, 1080)
+        await captureImage(`${BASE_URL}/imagegenerator/matchsummary/${matchid}`, "match_"+matchid, "#to_image", 1920, 1080)
         return fs.readFileSync(filepath)
     }
     return fs.readFileSync(filepath)
